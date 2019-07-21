a1f5ae8b4b48736ef4b98fd1786bb82c484764a0
tkarling <tkarling@live.com>
2019-07-21 15:10:37 -0700

    isMounted Test updates

--- src/containers/classicContainers/IsMountedTest.tsx
+++ src/containers/classicContainers/IsMountedTest.tsx
@@ -5,32 +5,54 @@ const myFetch: () => Promise<string> = () =>
     setTimeout(() => resolve('This text is the result'), 3000)
   )
 
-class CalleeWithIsMounted extends Component<{}, { saveMe: string }> {
-  state = { saveMe: '' }
-  _isMounted = false
+const Callee = ({ result, title }: { result: string; title: string }) => {
+  return (
+    <div style={{ border: '1px solid blue', padding: 8, marginTop: 8 }}>
+      <div style={{ padding: '8px 0px', fontWeight: 'bold' }}>{title}</div>
+      {result && <div>result: {result}</div>}
+      {!result && <div>Loading...</div>}
+    </div>
+  )
+}
 
+class CalleeWithWarning extends Component<{}, { savedResult: string }> {
+  state = { savedResult: '' }
+
+  componentDidMount() {
+    myFetch().then(result => {
+      this.setState({ savedResult: result }, () => {
+        console.log('savedResult', this.state.savedResult)
+      })
+    })
+  }
+  render() {
+    return (
+      <Callee result={this.state.savedResult} title="Callee with Warning" />
+    )
+  }
+}
+
+class CalleeWithIsMounted extends Component<{}, { savedResult: string }> {
+  state = { savedResult: '' }
+
+  _isMounted = false
   componentDidMount() {
     this._isMounted = true
     myFetch().then(result => {
       if (this._isMounted) {
-        this.setState({ saveMe: result }, () => {
-          console.log('saveMe', this.state.saveMe)
+        this.setState({ savedResult: result }, () => {
+          console.log('savedResult', this.state.savedResult)
         })
       }
     })
   }
-
   componentWillUnmount() {
     this._isMounted = false
   }
 
   render() {
-    const { saveMe } = this.state
     return (
-      <div style={{ border: '1px solid blue', padding: 8 }}>
-        Callee with _isMount
-        <div>saveMe: {saveMe}</div>
-      </div>
+      <Callee result={this.state.savedResult} title="Callee with _isMount" />
     )
   }
 }
@@ -51,23 +73,24 @@ const makeCancelable = (promise: any) => {
   return {
     promise: wrappedPromise,
     cancel() {
-      console.log('setting true')
       hasCanceled_ = true
     }
   }
 }
 
-class CalleeWithCancellablePromise extends Component<{}, { saveMe: string }> {
-  state = { saveMe: '' }
+class CalleeWithCancellablePromise extends Component<
+  {},
+  { savedResult: string }
+> {
+  state = { savedResult: '' }
 
   cancelablePromise = makeCancelable(myFetch())
 
   componentDidMount() {
     this.cancelablePromise.promise
       .then(result => {
-        console.log('resolved', result)
-        this.setState({ saveMe: result as string }, () => {
-          console.log('saveMe', this.state.saveMe)
+        this.setState({ savedResult: result as string }, () => {
+          console.log('savedResult', this.state.savedResult)
         })
       })
       .catch(reason => console.log('isCanceled', reason.isCanceled))
@@ -78,28 +101,48 @@ class CalleeWithCancellablePromise extends Component<{}, { saveMe: string }> {
   }
 
   render() {
-    const { saveMe } = this.state
     return (
-      <div style={{ border: '1px solid blue', padding: 8 }}>
-        Callee with cancelled Promise
-        <div>saveMe: {saveMe}</div>
-      </div>
+      <Callee
+        result={this.state.savedResult}
+        title="Callee with cancelled Promise"
+      />
     )
   }
 }
 
-class IsMountedTest extends Component<{}, { show: boolean }> {
-  state = { show: false }
+class IsMountedTest extends Component<
+  {},
+  { show: boolean; current: '_isMounted' | 'cancellable' | 'warning' }
+> {
+  state = {
+    show: false,
+    current: 'warning' as '_isMounted' | 'cancellable' | 'warning'
+  }
 
   toggleShow = () => this.setState({ show: !this.state.show })
 
+  toggleCallee = () =>
+    this.setState(state => ({
+      current:
+        state.current === 'warning'
+          ? '_isMounted'
+          : state.current === '_isMounted'
+          ? 'cancellable'
+          : 'warning'
+    }))
+
   render() {
-    const { show } = this.state
+    const { show, current } = this.state
+    const title = `Call with ${current}`
+
     return (
-      <div style={{ border: '1px solid red', padding: 8 }}>
-        <div>Caller</div>
+      <div style={{ border: '1px solid red', padding: 8, marginTop: 8 }}>
+        <button onClick={this.toggleCallee}>Swap Callee</button>
+        <div style={{ padding: '8px 0px', fontWeight: 'bold' }}>{title}</div>
         <button onClick={this.toggleShow}>Toggle</button>
-        {show && <CalleeWithCancellablePromise />}
+        {show && current === 'warning' && <CalleeWithWarning />}
+        {show && current === '_isMounted' && <CalleeWithIsMounted />}
+        {show && current === 'cancellable' && <CalleeWithCancellablePromise />}
       </div>
     )
   }
